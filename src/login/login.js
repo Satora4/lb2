8b482771025e8acf8990d1daa5279e7885265684
jvontobe <joel.vontobel@ergon.ch>
2022-09-15 16:28:43 +0200

    redirect added to login.js

--- src/login/login.js
+++ src/login/login.js
@@ -1,59 +1,59 @@
-/**
-
 import React from "react";
 
 import API from '../personList/api';
 
 export default class PersonList extends React.Component {
     state = {
-        persons: [],
         pw: "",
-        id: 0
+        id: undefined
     }
 
-    componentDidMount() {
+    handleId = event => {
+        this.setState({id: event.target.value})
+        console.log(this.state, "handleId")
+    }
+
+    handlePw = event => {
+        this.setState({pw: event.target.value})
+        console.log(this.state, "handlePw")
+    }
+
+    handleSubmit = event => {
+        event.preventDefault();
         console.log("before API");
-        API.get(`users`)
+        const {id, pw} = this.state;
+        API.get(`users/${id}`)
             .then(res => {
-                const persons = res.data;
-                this.setState({persons});
-                console.log("after API", this.state.persons);
+                if (id === undefined || null || pw === undefined || null){
+                    alert("Not everything filled out");
+                } else {
+                    const person = res.data;
+                    console.log(person.pw, pw);
+                    if (person.pw.toString() === pw.toString()) {
+                        console.log("Loged in");
+                        window.location.href = "http://localhost:3001/task";
+                    } else {
+                        alert("Wrong password oder userId");
+                    }
+                }
+
             })
     }
 
     render() {
         console.log("render()");
-        if (this.state.persons !== undefined && this.state.persons.length >= 1) {
-            const id = this.checkUser();
-            return (
-                <ul>
-                    {this.state.persons?.map(person => person[id].user?.task.map(task => <li>{task}</li>))}
-                </ul>
-            )
-        }
-    }
+        return (
+            <dif className="form">
+                <form onSubmit={this.handleSubmit}>
+                    <label>Enter your Id: </label><br/>
+                    <input type="number" autoComplete="off" name="userId" onChange={this.handleId}/><br/>
+                    <label>Enter your Password: </label><br/>
+                    <input type="text" autoComplete="off" name="pw" onChange={this.handlePw}/><br/>
 
-    idNumber() {
-        console.log("idNumber");
-        return prompt("gib deine id an: ");
-    }
+                    <button type="submit">Login</button>
+                </form>
+            </dif>
+        )
 
-    pw() {
-        console.log("pw");
-        return prompt("gib dein Passwort an: ");
-    }
-
-    checkUser() {
-        let pw = undefined;
-        let id = undefined;
-
-        while (!(this.persons?.map(person => person.user.at(id).pw) === pw)) {
-            id = this.idNumber();
-            pw = this.pw();
-            console.log(pw);
-            console.log(this.persons?.map(person => person.user.at(id).pw));
-        }
-        return id;
     }
 }
-**/
